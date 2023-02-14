4585a538e4a62309dce1f64d2f2c177f77f2e1b0
Doston Xusanov <algorberun@gmail.com>
2023-02-14 19:47:48 +0500

    props basic2

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 
-export default function Main(props) {
-  console.log("props", props);
-
+export default function Main({ visible, count }) {
   return (
     <div>
       <h1>Main</h1>
+      <h3>Visible: {JSON.stringify({ visible, count })}</h3>
     </div>
   );
 }
