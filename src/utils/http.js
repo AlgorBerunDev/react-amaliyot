d3a1726640c10b456a5c7e25a34d80fa51630512
Doston Xusanov <algorberun@gmail.com>
2023-03-02 19:48:09 +0500

    Added axios

---
+++ src/utils/http.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const handleSuccess = (response, successHandler, finishHandler, dispatch, showSuccessToast) => {
+  if (successHandler) successHandler(response);
+  if (finishHandler) finishHandler(response);
+  return response;
+};
+
+const handleError = (err, errorHandler, finishHandler, dispatch) => {
+  console.error(err?.response?.status);
+};
+
+export default async function http({ url, method, data, success, error, finish, dispatch, showSuccessToast = false }) {
+  const response = await axios({ url, method, data })
+    .then(res => handleSuccess(res, success, finish, dispatch, showSuccessToast))
+    .catch(err => handleError(err, error, finish, dispatch));
+
+  return {
+    json: response && response.data,
+    status: response && response.status,
+    response,
+  };
+}
