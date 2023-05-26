11b06bcd936ec9e2ab4acf6aee00d0cdf837ab46
doniyor-eshmamatov <edoniyorbekdev@gmail.com>
2023-05-26 21:05:43 +0500

    drop-n-drag-doniyor

--- src/pages/drag-n-drop.js
+++ src/pages/drag-n-drop.js
@@ -1,58 +1,80 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const Item = ({ id, text }) => {
-  const handleDragStart = event => {
-    console.log("DragStart", id);
-    event.dataTransfer.setData("text/plain", id);
-  };
+	const handleDragStart = (event) => {
+		console.log('DragStart', id);
+		event.dataTransfer.setData('text/plain', id);
+	};
 
-  return (
-    <div draggable onDragStart={handleDragStart} style={{ border: "2px solid green" }}>
-      {text}
-    </div>
-  );
+	return (
+		<div
+			draggable
+			onDragStart={handleDragStart}
+			style={{ border: '2px solid green' }}
+		>
+			{text}
+		</div>
+	);
 };
 
 const DropTarget = ({ onDrop, children }) => {
-  const handleDragOver = event => {
-    event.preventDefault();
-    event.dataTransfer.dropEffect = "move";
-  };
+	const handleDragOver = (event) => {
+		event.preventDefault();
+		event.dataTransfer.dropEffect = 'move';
+	};
 
-  const handleDrop = event => {
-    event.preventDefault();
-    const id = event.dataTransfer.getData("text/plain");
-    console.log("handleDrop", id);
-    onDrop(id);
-  };
+	const handleDrop = (event) => {
+		event.preventDefault();
+		const id = event.dataTransfer.getData('text/plain');
+		console.log('handleDrop', id);
+		onDrop(id);
+	};
 
-  return (
-    <div style={{ border: "2px solid red" }} onDragOver={handleDragOver} onDrop={handleDrop}>
-      {children}
-    </div>
-  );
+	return (
+		<div
+			style={{ border: '2px solid red' }}
+			onDragOver={handleDragOver}
+			onDrop={handleDrop}
+		>
+			{children}
+		</div>
+	);
 };
 
 const DragAndDrop = () => {
-  const [items, setItems] = useState([
-    { id: "item1", text: "Item 1" },
-    { id: "item2", text: "Item 2" },
-    { id: "item3", text: "Item 3" },
-  ]);
+	const [items, setItems] = useState([
+		{ id: 'item1', text: 'Item 1' },
+		{ id: 'item2', text: 'Item 2' },
+		{ id: 'item3', text: 'Item 3' },
+	]);
 
-  const handleDrop = id => {
-    const updatedItems = items.filter(item => item.id !== id);
-    setItems(updatedItems);
-  };
+	const [items2, setItems2] = useState([]);
 
-  return (
-    <>
-      {items.map(item => (
-        <Item key={item.id} id={item.id} text={item.text} />
-      ))}
-      <DropTarget onDrop={handleDrop}>Drop here</DropTarget>
-    </>
-  );
+	const handleDrop = (id) => {
+		const updatedItems = items.filter((item) => {
+      if(item.id === id) {
+        setItems2([...items2, ...[item]])
+        return false
+      }
+      return true;
+    });
+
+		setItems(updatedItems);
+	};
+
+	return (
+		<>
+			{items.map((item) => (
+				<Item key={item.id} id={item.id} text={item.text} />
+			))}
+			<DropTarget onDrop={handleDrop}>
+				<div>Drop here</div>
+				{items2.map((item) => (
+				<Item key={item.id} id={item.id} text={item.text} />
+			))}
+			</DropTarget>
+		</>
+	);
 };
 
 export default DragAndDrop;
